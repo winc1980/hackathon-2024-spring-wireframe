09b5080ecaddb44900317eda3251fe3023a27b23
kanako0424 <budoukan0424@gmail.com>
2024-02-24 18:22:29 +0900

    ローカルで動かす時の手順を記載

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hackathon-2024-spring-wireframe
+
+
+# ローカル環境でテストする場合のmysql
+0. mysqlをダウンロードする。Macユーザーでhomebrew使っているならHomeberwでダウンロードしてパスを通す
+1. pip install requirements.txtをしてrequirements.txtにあるライブラリを全てダウンロードする
+2. randaction という名前のdatabaseを作る(CREATE DATABASEコマンド)
+3. USE randaction;
+4. create_db.pyを実行する
+5. mysqlで、SHOW tablesをしてtableが作成されたかを確認する
--- __init__.py
+++ __init__.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
 # pythonanywhereでデプロイする場合のmysql
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://randaction:2024-spring-hackathon-group2@randaction.mysql.pythonanywhere-services.com/randaction' 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://randaction:2024-spring-hackathon-group2@randaction.mysql.pythonanywhere-services.com/randaction$randaction' 
 
 #ローカル環境でテストする場合のmysql
+# 0. mysqlをダウンロードする。Macユーザーでhomebrew使っているならHomeberwでダウンロードしてパスを通す
+# pip install requirements.txtをしてrequirements.txtにあるライブラリを全てダウンロードする
+# 1. randaction という名前のdatabaseを作る(CREATE DATABASEコマンド)
+# 2. create_db.pyを実行する
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/randaction' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
