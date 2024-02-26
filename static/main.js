006e504da6d8db84deca5b96be30a249d1c16525
kanako0424 <budoukan0424@gmail.com>
2024-02-26 11:58:52 +0900

    fix

---
+++ static/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var toSigninButton = document.getElementById('to-signin-button');
+    var toLoginButton = document.getElementById('to-login-button');
+    var loginWrapper = document.getElementById('login-wrapper');
+    var signinWrapper = document.getElementById('signin-wrapper');
+
+    loginWrapper.style.display = 'none';
+    signinWrapper.style.display = 'block';
+
+    // サインインボタンがクリックされたとき
+    toSigninButton.addEventListener('click', function() {
+        loginWrapper.style.display = 'none';
+        signinWrapper.style.display = 'block';
+    });
+
+    // ログインボタンがクリックされたとき
+    toLoginButton.addEventListener('click', function() {
+        signinWrapper.style.display = 'none';
+        loginWrapper.style.display = 'block';
+    });
+});
